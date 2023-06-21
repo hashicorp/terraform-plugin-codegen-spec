aad032aa23dd6fd90b2e5e7809d350bc0c7a2704
Brian Flad <bflad417@gmail.com>
2023-06-21 09:13:13 -0400

    spec: Add ObjectType with CustomType
    Reference: https://github.com/hashicorp/terraform-plugin-codegen-spec/issues/25
    
    This breaking change ensures there is consistency that specification implementations can use `CustomType` with all types, in this case object types underlying any attribute type.
    
    For existing implementations, such as:
    
    ```json
    {
                "name": "list_object_attribute",
                "list": {
                  "computed_optional_required": "computed",
                  "element_type": {
                    "object": [
                      {
                        "name": "obj_string_attr",
                        "string": {}
                      }
                    ]
                  }
                }
              },
    ```
    
    Are now defined as:
    
    ```json
    {
                "name": "list_object_attribute",
                "list": {
                  "computed_optional_required": "computed",
                  "element_type": {
                    "object": {
                      "attribute_types": [
                        {
                          "name": "obj_string_attr",
                          "string": {}
                        }
                      ]
                    }
                  }
                }
              },
    ```
    
    Note that there is now a slight inconsistency between how `CustomType` is defined on collection types (`ListType`, `MapType`, and `SetType`) that are already element types and need to include their own element type. The `CustomType` is specified along side the element type, rather than explicitly having an `ElementType` delineation.
    
    Currently:
    
    ```json
              {
                "name": "map_list_string_attribute",
                "map": {
                  "computed_optional_required": "computed",
                  "element_type": {
                    "list": {
                      "custom_type": {
                        "import": "github.com/hashicorp/terraform-plugin-framework/types/basetypes",
                        "type": "basetypes.ListType",
                        "value_type": "basetypes.ListValue"
                      },
                      "string": {}
                    }
                  }
                }
              },
    ```
    
    It may make sense to reintroduce what was originally designed in the specification with the verbose `ElementType` property for clarity.
    
    ```json
              {
                "name": "map_list_string_attribute",
                "map": {
                  "computed_optional_required": "computed",
                  "element_type": {
                    "list": {
                      "custom_type": {
                        "import": "github.com/hashicorp/terraform-plugin-framework/types/basetypes",
                        "type": "basetypes.ListType",
                        "value_type": "basetypes.ListValue"
                      },
                      "element_type": {
                        "string": {}
                      }
                    }
                  }
                }
              },
    ```

--- schema/element_type.go
+++ schema/element_type.go
@@ -4,13 +4,13 @@
 package schema
 
 type ElementType struct {
-	Bool    *BoolType             `json:"bool,omitempty"`
-	Float64 *Float64Type          `json:"float64,omitempty"`
-	Int64   *Int64Type            `json:"int64,omitempty"`
-	List    *ListType             `json:"list,omitempty"`
-	Map     *MapType              `json:"map,omitempty"`
-	Number  *NumberType           `json:"number,omitempty"`
-	Object  []ObjectAttributeType `json:"object,omitempty"`
-	Set     *SetType              `json:"set,omitempty"`
-	String  *StringType           `json:"string,omitempty"`
+	Bool    *BoolType    `json:"bool,omitempty"`
+	Float64 *Float64Type `json:"float64,omitempty"`
+	Int64   *Int64Type   `json:"int64,omitempty"`
+	List    *ListType    `json:"list,omitempty"`
+	Map     *MapType     `json:"map,omitempty"`
+	Number  *NumberType  `json:"number,omitempty"`
+	Object  *ObjectType  `json:"object,omitempty"`
+	Set     *SetType     `json:"set,omitempty"`
+	String  *StringType  `json:"string,omitempty"`
 }
