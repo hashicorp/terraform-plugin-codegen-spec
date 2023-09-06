a7ebdddc2f27f1b254f17e46f6e64eb6bf419319
Benjamin Bennett <ben.bennett@hashicorp.com>
2023-09-06 11:53:56 +0100

    Add equality function to schema.CustomType

--- schema/custom_type.go
+++ schema/custom_type.go
@@ -11,6 +11,61 @@ type CustomType struct {
 	ValueType string       `json:"value_type"`
 }
 
-func (c CustomType) HasImport() bool {
+func (c *CustomType) HasImport() bool {
 	return c.Import != nil && c.Import.Path != ""
+}
+
+func (c *CustomType) Equal(other *CustomType) bool {
+	if c == nil && other == nil {
+		return true
+	}
+
+	if c == nil && other != nil {
+		return false
+	}
+
+	if c != nil && other == nil {
+		return false
+	}
+
+	if c.Import == nil && other.Import != nil {
+		return false
+	}
+
+	if c.Import != nil && other.Import == nil {
+		return false
+	}
+
+	if c.Import != nil && other.Import != nil {
+		cImport := *c.Import
+		otherImport := *other.Import
+
+		if cImport.Alias == nil && otherImport.Alias != nil {
+			return false
+		}
+
+		if cImport.Alias != nil && otherImport.Alias == nil {
+			return false
+		}
+
+		if cImport.Alias != nil && otherImport.Alias != nil {
+			if *cImport.Alias != *otherImport.Alias {
+				return false
+			}
+		}
+
+		if cImport.Path != otherImport.Path {
+			return false
+		}
+	}
+
+	if c.Type != other.Type {
+		return false
+	}
+
+	if c.ValueType != other.ValueType {
+		return false
+	}
+
+	return true
 }
--- schema/custom_type_test.go
+++ schema/custom_type_test.go
@@ -16,15 +16,15 @@ func TestCustomType_HasImport(t *testing.T) {
 	t.Parallel()
 
 	testCases := map[string]struct {
-		customType schema.CustomType
+		customType *schema.CustomType
 		expected   bool
 	}{
 		"import-nil": {
-			customType: schema.CustomType{},
+			customType: &schema.CustomType{},
 			expected:   false,
 		},
 		"import-empty-string": {
-			customType: schema.CustomType{
+			customType: &schema.CustomType{
 				Import: &code.Import{
 					Path: "",
 				},
@@ -32,7 +32,7 @@ func TestCustomType_HasImport(t *testing.T) {
 			expected: false,
 		},
 		"import-string": {
-			customType: schema.CustomType{
+			customType: &schema.CustomType{
 				Import: &code.Import{
 					Path: "github.com/owner/repo/pkg",
 				},
@@ -48,6 +48,177 @@ func TestCustomType_HasImport(t *testing.T) {
 			t.Parallel()
 
 			got := testCase.customType.HasImport()
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestCustomType_Equal(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		customType *schema.CustomType
+		other      *schema.CustomType
+		expected   bool
+	}{
+		"nil_other_not_nil": {
+			other:    &schema.CustomType{},
+			expected: false,
+		},
+		"not_nil_other_nil": {
+			customType: &schema.CustomType{},
+			expected:   false,
+		},
+		"import_nil_other_import_not_nil": {
+			customType: &schema.CustomType{},
+			other: &schema.CustomType{
+				Import: &code.Import{},
+			},
+			expected: false,
+		},
+		"import_not_nil_other_import_nil": {
+			customType: &schema.CustomType{
+				Import: &code.Import{},
+			},
+			other:    &schema.CustomType{},
+			expected: false,
+		},
+		"import_alias_nil_other_import_alias_not_nil": {
+			customType: &schema.CustomType{
+				Import: &code.Import{},
+			},
+			other: &schema.CustomType{
+				Import: &code.Import{
+					Alias: pointer("alias"),
+				},
+			},
+			expected: false,
+		},
+		"import_alias_not_nil_other_import_alias_nil": {
+			customType: &schema.CustomType{
+				Import: &code.Import{
+					Alias: pointer("alias"),
+				},
+			},
+			other: &schema.CustomType{
+				Import: &code.Import{},
+			},
+			expected: false,
+		},
+		"import_path_empty_other_import_path_not_empty": {
+			customType: &schema.CustomType{
+				Import: &code.Import{},
+			},
+			other: &schema.CustomType{
+				Import: &code.Import{
+					Path: "path",
+				},
+			},
+			expected: false,
+		},
+		"import_path_not_empty_other_import_path_empty": {
+			customType: &schema.CustomType{
+				Import: &code.Import{
+					Path: "path",
+				},
+			},
+			other: &schema.CustomType{
+				Import: &code.Import{},
+			},
+			expected: false,
+		},
+		"import_match": {
+			customType: &schema.CustomType{
+				Import: &code.Import{
+					Alias: pointer("alias"),
+					Path:  "path",
+				},
+			},
+			other: &schema.CustomType{
+				Import: &code.Import{
+					Alias: pointer("alias"),
+					Path:  "path",
+				},
+			},
+			expected: true,
+		},
+		"type_empty_other_type_not_empty": {
+			customType: &schema.CustomType{},
+			other: &schema.CustomType{
+				Type: "type",
+			},
+			expected: false,
+		},
+		"type_not_empty_other_type_empty": {
+			customType: &schema.CustomType{
+				Type: "type",
+			},
+			other:    &schema.CustomType{},
+			expected: false,
+		},
+		"type_match": {
+			customType: &schema.CustomType{
+				Type: "type",
+			},
+			other: &schema.CustomType{
+				Type: "type",
+			},
+			expected: true,
+		},
+		"value_type_empty_other_value_type_not_empty": {
+			customType: &schema.CustomType{},
+			other: &schema.CustomType{
+				ValueType: "valueType",
+			},
+			expected: false,
+		},
+		"value_type_not_empty_other_value_type_empty": {
+			customType: &schema.CustomType{
+				ValueType: "valueType",
+			},
+			other:    &schema.CustomType{},
+			expected: false,
+		},
+		"value_type_match": {
+			customType: &schema.CustomType{
+				ValueType: "valueType",
+			},
+			other: &schema.CustomType{
+				ValueType: "valueType",
+			},
+			expected: true,
+		},
+		"match": {
+			customType: &schema.CustomType{
+				Import: &code.Import{
+					Alias: pointer("alias"),
+					Path:  "path",
+				},
+				Type:      "type",
+				ValueType: "valueType",
+			},
+			other: &schema.CustomType{
+				Import: &code.Import{
+					Alias: pointer("alias"),
+					Path:  "path",
+				},
+				Type:      "type",
+				ValueType: "valueType",
+			},
+			expected: true,
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.customType.Equal(testCase.other)
 
 			if diff := cmp.Diff(got, testCase.expected); diff != "" {
 				t.Errorf("unexpected difference: %s", diff)
