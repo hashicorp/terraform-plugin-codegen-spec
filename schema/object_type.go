6c02cbd06801b86a60a8281d1eba9ff94554079c
Benjamin Bennett <ben.bennett@hashicorp.com>
2023-09-12 13:28:47 +0100

    Adding Equal() methods on schema.ElementType and schema.ObjectType

--- schema/object_type.go
+++ schema/object_type.go
@@ -9,3 +9,61 @@ type ObjectType struct {
 	// CustomType is a customization of the ObjectType.
 	CustomType *CustomType `json:"custom_type,omitempty"`
 }
+
+func (o *ObjectType) Equal(other *ObjectType) bool {
+	if o == nil && other == nil {
+		return true
+	}
+
+	if o == nil || other == nil {
+		return false
+	}
+
+	if !o.CustomType.Equal(other.CustomType) {
+		return false
+	}
+
+	if o.AttributeTypes == nil && other.AttributeTypes != nil {
+		return false
+	}
+
+	if o.AttributeTypes != nil && other.AttributeTypes == nil {
+		return false
+	}
+
+	for k, v := range o.AttributeTypes {
+		if v.Name != other.AttributeTypes[k].Name {
+			return false
+		}
+
+		a := ElementType{
+			Bool:    v.Bool,
+			Float64: v.Float64,
+			Int64:   v.Int64,
+			List:    v.List,
+			Map:     v.Map,
+			Number:  v.Number,
+			Object:  v.Object,
+			Set:     v.Set,
+			String:  v.String,
+		}
+
+		b := ElementType{
+			Bool:    other.AttributeTypes[k].Bool,
+			Float64: other.AttributeTypes[k].Float64,
+			Int64:   other.AttributeTypes[k].Int64,
+			List:    other.AttributeTypes[k].List,
+			Map:     other.AttributeTypes[k].Map,
+			Number:  other.AttributeTypes[k].Number,
+			Object:  other.AttributeTypes[k].Object,
+			Set:     other.AttributeTypes[k].Set,
+			String:  other.AttributeTypes[k].String,
+		}
+
+		if !a.Equal(b) {
+			return false
+		}
+	}
+
+	return true
+}
